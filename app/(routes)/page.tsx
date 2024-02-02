857bf7bfb5c1e34a38e7cd3ebdf01c2dbc4e81c2
Calda Joselito Jr Lopez <joselito.calda@DSB.com>
2024-02-02 17:46:54 +0800

    initial commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./page.module.css";
+
+const HomePage = () => {
+  return <main className={styles.homePage}>HomePage</main>;
+};
+
+export default HomePage;
---
+++ app/(routes)/profile/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./page.module.css";
+
+const ProfilePage = () => {
+  return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
