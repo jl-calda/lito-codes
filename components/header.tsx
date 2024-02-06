17187c084698f7df2eaca51201d31bb5ee355316
JLCalda <jlcaldadev23@gmail.com>
2024-02-07 07:01:10 +0800

    added tailwind

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,30 @@
-import { SunIcon } from "@radix-ui/react-icons";
-import styles from "./header.module.scss";
+"use client";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 export const Header = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <header className={styles.header}>
+    <header className="flex items-center justify-between">
       <div>
         <p>Logo</p>
       </div>
       <ul>
         <li>
-          <SunIcon />
+          <Button
+            variant="ghost"
+            // asChild
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {theme === "dark" ? (
+              <SunIcon className="w-4 h-4" />
+            ) : (
+              <MoonIcon className="w-4 h-4" />
+            )}
+          </Button>
         </li>
       </ul>
     </header>
