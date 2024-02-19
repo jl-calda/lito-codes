13793556a30ef38a357fd156dfe3b081d519ba6c
Calda Joselito Jr Lopez <joselito.calda@DSB.com>
2024-02-19 15:53:19 +0800

    fixed sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,7 +15,6 @@ import { Card, CardContent, CardHeader } from "./ui/card";
 import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { Separator } from "./ui/separator";
 import { ReactElement } from "react";
 import { Input } from "./ui/input";
 import { useMediaQuery } from "@uidotdev/usehooks";
@@ -26,61 +25,70 @@ type NavItem = {
   url: string;
   icon: ReactElement<any, any>;
   live: boolean;
+  active?: boolean;
 };
-
-const navitems: NavItem[] = [
-  {
-    name: "Home",
-    url: "/",
-    icon: <FaHouse className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "About",
-    url: "/about",
-    icon: <FaUserLarge className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "Projects",
-    url: "/projects",
-    icon: <FaCubesStacked className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "Services",
-    url: "/services",
-    icon: <FaHandHolding className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "Contact",
-    url: "/contact",
-    icon: <FaMessage className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "Blog",
-    url: "/blog",
-    icon: <FaBlogger className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "Code",
-    url: "/code",
-    icon: <FaCode className="h-4 w-4 mr-2" />,
-    live: true,
-  },
-  {
-    name: "Merch",
-    url: "/merch",
-    icon: <FaShirt className="h-4 w-4 mr-2" />,
-    live: false,
-  },
-];
 
 export const Sidebar = () => {
   const pathName = usePathname();
+  const navitems: NavItem[] = [
+    {
+      name: "Home",
+      url: "/",
+      icon: <FaHouse className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName === "/",
+    },
+    {
+      name: "About",
+      url: "/about",
+      icon: <FaUserLarge className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName.startsWith("/about"),
+    },
+    {
+      name: "Projects",
+      url: "/projects",
+      icon: <FaCubesStacked className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName.startsWith("/projects"),
+    },
+    {
+      name: "Services",
+      url: "/services",
+      icon: <FaHandHolding className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName.startsWith("/services"),
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+      icon: <FaMessage className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName.startsWith("/contact"),
+    },
+    {
+      name: "Blog",
+      url: "/blog",
+      icon: <FaBlogger className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName.startsWith("/blog"),
+    },
+    {
+      name: "Code",
+      url: "/code",
+      icon: <FaCode className="h-4 w-4 mr-2" />,
+      live: true,
+      active: pathName.startsWith("/code"),
+    },
+    {
+      name: "Merch",
+      url: "/merch",
+      icon: <FaShirt className="h-4 w-4 mr-2" />,
+      live: false,
+      active: pathName.startsWith("/merch"),
+    },
+  ];
+
   const isMobile = useMediaQuery("only screen and (max-width : 768px)");
   return (
     <aside className="flex flex-col space-y-2">
@@ -105,7 +113,7 @@ export const Sidebar = () => {
         <div className="flex flex-wrap gap-x-2 gap-y-2">
           {navitems.map((item) => (
             <Button
-              variant={pathName === item.url ? "outline" : "secondary"}
+              variant={item.active ? "outline" : "secondary"}
               size={isMobile ? "sm" : "default"}
               className=""
               key={crypto.randomUUID()}
